7ba2326ee190d3a39f4adc655d78d79ea97446f0
RovarQRoom <rovar.07000120@outlook.com>
2023-05-01 13:27:31 +0300

    Fixed Cache And Now Time For Clearing

--- Module/Redis.module.ts
+++ Module/Redis.module.ts
@@ -1,28 +1,52 @@
-import redis from 'redis';
+
+// import {   } from '@types/redis';
 import { promisify } from 'util';
+import { RedisClientType, createClient } from 'redis';
 
 class RedisModule {
-  private client;
+  private client: RedisClientType | undefined;
 
-  constructor() {
-    this.client = redis.createClient({
-      url: "redis://localhost:6379"
-    });
-    this.client.on('error', (error:any) => console.error(error));
+   constructor() {
+ this.connectToRedis();
+    }
+
+  async connectToRedis() {
+    try{
+      this.client = createClient({
+        url: "redis://localhost:6379"
+      });
+      await this.client.connect();
+      this.client.on('error', (error:any) => console.error('redis error',error));
+      this.client.on('connect', () => console.log('Redis connected'))
+    }catch(err){
+      console.log("Dat Error",err);
+      console.log("RedisModule" , this.client);
+      
+    }
   }
+    
 
-  async getAsync(key: string): Promise<string | null> {
-    const asyncGet = promisify(this.client.get).bind(this.client);
-    return asyncGet(key);
+  async getAsync(key: string) {
+    if(!this.client) return null;
+    try{
+      const asyncGet = await this.client.get(key);
+      return asyncGet;
+    }catch (err){
+      console.log("getAsync Error",err); 
+      return null;
+    }
   }
 
   async setAsync(key: string, value: any, expiration?: number): Promise<boolean> {
-    const asyncSet = promisify(this.client.set).bind(this.client);
-    let result = await asyncSet(key, value);
-    if (expiration) {
-      result = await this.client.expire(key, expiration);
-    }
-    return result === 1;
+    if(!this.client) return false;
+    const asyncSet = await this.client.set(key, value);
+    return true;
+  }
+
+  async clearAsync(){
+    if(!this.client) return false;
+    await this.client.flushAll();
+    return true;
   }
 }
 
--- Repository/Search.repository.ts
+++ Repository/Search.repository.ts
@@ -15,12 +15,21 @@ export class SearchRepository implements ISearchRepository {
   async find(search: any): Promise<Document[]> {
     try {
       const searchCache = await RedisModule.getAsync("search");
-      if(searchCache) return JSON.parse(searchCache);
+      const searchJSON = JSON.parse(searchCache || '');
+      console.log("searchJSON",searchJSON[0].Name);
 
-      const searchResult = await this.searchModel.find({Name:{$regex:new RegExp(".*"+search+".*",'i')}}).limit(7).exec();
-      await RedisModule.setAsync("search", JSON.stringify(searchResult));
+      console.log(searchJSON[0].Name === search);
+      
+      if(searchCache) {
+        console.log("Congratulation, you are using cache");
+      return searchJSON;
+      }else{
+        await RedisModule.clearAsync();
+        const searchResult = await this.searchModel.find({Name:{$regex:new RegExp(".*"+search+".*",'i')}}).limit(7).exec();
+        await RedisModule.setAsync("search", JSON.stringify(searchResult), 60);
+        return searchResult;
+      }
 
-      return searchResult;
     } catch (error) {
       throw new Error(`Failed to get search result: ${error}`);
     }
